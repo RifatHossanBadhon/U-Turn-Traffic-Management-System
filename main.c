b239a455678c5385378c17488ad113814e51214c
Rifat Hossan Badhon <161221951+RifatHossanBadhon@users.noreply.github.com>
2024-12-11 22:02:25 +0600

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,12 @@
-
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <limits.h>
+#define i_size 60
+#define NAME 100
+#define PASS 8
+#define MAX_CITIES 1000
+#define NAME_LENGTH 50
+#define MAX_SIGNALS 10
